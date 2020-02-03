becfeb17dddca11f8588a6322be5e9d7312be27b
Amory Galili <amorygalili@gmail.com>
2020-02-03 18:14:53 -0500

    Updated dependencies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
+  input: './src/index.js',
   output: {
+    file: './dist/webbit-store.js',
+    format: 'umd',
+    name: 'WebbitStore',
     globals: {
       'redux': 'Redux',
       'lodash': '_'
     }
-  }
+  },
+  plugins: [
+    babel()
+  ]
 }
